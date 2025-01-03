58a5e7fcb36cb236cb560966f78ce7fcc4142bc8
amitverma-cf <ava.amitverma@gmail.com>
2025-01-04 03:41:00 +0530

    Fixed mobile responsiveness in about title

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import About from "@/components/app/About";
 export default function AboutPage() {
     return (
         <>
-            <div className='text-8xl md:text-9xl font-heading p-28 text-center bg-pink-300'>About</div>
+            <div className='text-7xl md:text-9xl font-heading p-28 text-center bg-pink-300'>About</div>
             <About />
         </>
     );
